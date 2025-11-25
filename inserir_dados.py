aec9562e4fa000c0268d8ee5e8b8d87bc31a3a22
danielmeneguelli <danielmeneguelli1@gmail.com>
2025-11-25 13:26:01 -0300

    Update inserir_dados.py
    inserção de dados na tabela pedidos.

--- inserir_dados.py
+++ inserir_dados.py
@@ -10,7 +10,7 @@ try:
                         ('Daniel Campos', 'danielsouza@gmail.com', 23 ),
                         ('Jose Junior', 'josejunior@gmail.com', 32),
                         ('Julia almeida', 'juliaalmeida@gmail.com', 56)
-                    """
+                       """
     
     inserir_produtos = """INSERT INTO produtos 
                         (nome, preco, estoque)
@@ -18,10 +18,19 @@ try:
                         ('carregador', 50, 100),
                         ('monitor', 899.99, 15), 
                         ('mouse', 149.90, 25)
-                    """
+                       """
+    
+    inserir_pedidos = """INSERT INTO pedidos
+                        (cliente_id, produto_id, quantidade, total)
+                        VALUES
+                        (1, 2, 1, 899.99),
+                        (3, 3, 2, 299.80), 
+                        (2, 1, 5, 250)
+                      """
     cursor = con.cursor()
     cursor.execute(inserir_clientes)
     cursor.execute(inserir_produtos)
+    cursor.execute(inserir_pedidos)
     con.commit()
     print(cursor.rowcount, 'registros inseridos na tabela!')
     cursor.close()
